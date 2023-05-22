ac2fa447cfa6b05c2e46494448291d2df5d15eb6
MihaelaAntalute <antalutemihaela32@gmail.com>
2023-05-22 20:02:11 +0300

    first commit

---
+++ discord4j/src/main/java/discord4j/Discord4jApplication.java
@@ -0,0 +1,13 @@
+package discord4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Discord4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Discord4jApplication.class, args);
+	}
+
+}
